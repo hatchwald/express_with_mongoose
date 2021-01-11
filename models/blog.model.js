8291eda5d447e11501df9710b3e4222034f46917
hatchwald <fatkhur139wec@gmail.com>
2021-01-11 15:24:25 +0700

    update data Blog

--- models/blog.model.js
+++ models/blog.model.js
@@ -4,18 +4,11 @@ const blog_format = new mongoose.Schema({
     author: { type: String, required: true },
     title: { type: String, required: true },
     body: { type: String, required: true },
-    date: { type: Date, default: Date.now },
-    id_blog: { type: String }
+    posting_date: { type: Date, default: Date.now },
+    date_modified: { type: Date, default: Date.now },
+    id_blog: { type: String, required: true }
 })
 
-blog_format.pre("save", function (next) {
-    const blog = this;
-    const time_stamp = Date.now()
-    const id_blog = blog.author + "-" + time_stamp;
-    blog.id_blog = id_blog
-
-    next()
-})
 const Blog = mongoose.model(
     "Blog", blog_format
 )
--- routes/api/blog/index.js
+++ routes/api/blog/index.js
@@ -21,10 +21,13 @@ router.get("/", function (req, res) {
     jwt.verify(bearer_value, process.env.SECRET, (err, decoded) => {
         username = decoded.id
     })
+    const time_stamp = Date.now()
+
     const new_blog = new blogData({
         author: username,
         title: form_data.title,
-        body: form_data.blog
+        body: form_data.blog,
+        id_blog: username + "-" + time_stamp
     })
 
     new_blog.save((err, new_blog) => {
@@ -44,6 +47,31 @@ router.get("/", function (req, res) {
             res.status(404).json({ message: "Blog data not found !" })
         } else {
             res.status(200).json({ data: doc, message: "found" })
+        }
+    })
+
+}).put("/:blogId", async (req, res) => {
+    const param = req.params
+    const idblog = param.blogId
+    const form_data = req.body;
+
+    blogData.findOne({ id_blog: idblog }, async (err, doc) => {
+        if (doc == null) {
+            res.status(404).json({ message: "Blog data not found !" })
+        } else {
+            doc.title = form_data.title;
+            doc.body = form_data.blog;
+            const date_now = new Date();
+            doc.date_modified = date_now.toISOString();
+
+            doc.save((err, new_data) => {
+                if (err) {
+                    res.status(500).send(err)
+                } else {
+                    res.status(200).json({ message: "Blog data updated" })
+                }
+            })
+
         }
     })
 
